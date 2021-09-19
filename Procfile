497a245b28425af4d2363c102b95dfd56fe2adcc
Passion4film <passion4film@hotmail.co.uk>
2021-09-19 10:11:03 +0000

    added allowed hosts and connecting to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn passion_4_painting.wsgi.application
--- passion_4_painting/settings.py
+++ passion_4_painting/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3t0_lv%rrkr@u#si2#y)+62)#@*(kx53#uzp*$6p9kr6jd13n%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['passion-4-painting.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,12 +119,17 @@ WSGI_APPLICATION = 'passion_4_painting.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
