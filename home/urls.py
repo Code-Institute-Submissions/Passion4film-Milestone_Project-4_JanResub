fbd410a9ae7575a976c867a675813106b0078210
Passion4film <passion4film@hotmail.co.uk>
2021-09-23 15:24:50 +0000

    added view and url for health_benefits and created template linked to nav bar

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
+    path('health_benefits/', views.health_benefits, name='health_benefits'),
     path('', views.index, name='home'),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,9 @@ def index(request):
     """ A view to return the index page """
     
     return render(request, 'home/index.html')
+
+
+def health_benefits(request):
+    """ A view to return the health and benefits page """
+    
+    return render(request, 'home/health_benefits.html')
